d7b96f711a502282f1f7ae35c76a20c6163bd935
TakumaTotani <t.totani08307@gmail.com>
2017-03-09 17:44:33 +0900

    基礎プログラミング学習の課題を提出いたします。

--- CampChallenge_PHP/003.プログラミング基礎学習３/Kiso_3_7.php
+++ CampChallenge_PHP/003.プログラミング基礎学習３/Kiso_3_7.php
@@ -7,16 +7,16 @@ function kakeru(){
 
   global $a;//グローバル変数として計算を行う変数
   $a=$a*2;//二倍かけていく
+  static $count = 0;//何回実行されているかを保持する変数
+  echo '回数'.$count++.'<br>';
   return $a;
-  static $b=0;//何回実行されているかを保持する変数
 }
 
 for($i=0;$i<20;$i++){//関数を20回呼び出している
-  $b=$i;//実行数を代入
   echo kakeru()."<br>";
 
 }
-  echo '実行されている回数は'.$i.'回です';
+
 
 
 
